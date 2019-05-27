905ab5c922f52a7eaa198aa1f64cdc3ff027d78c
gg-fu <13432820164@163.com>
2019-05-27 12:56:32 +0800

    add queue

---
+++ libevent-buffer/client/include/queue.h
@@ -0,0 +1,20 @@
+#ifndef _QUEUE_H
+#define _QUEUE_H
+#include <stdlib.h>
+#include <pthread.h>
+#define QUEUE_SIZE 20
+
+typedef struct {
+    void **data;
+    int _head;
+    int _tail;
+
+    pthread_mutex_t lock; /* 互斥体lock 用于对缓冲区的互斥操作 */
+    pthread_cond_t notempty; /* 缓冲区非空的条件变量 */
+    pthread_cond_t notfull; /* 缓冲区未满的条件变量 */
+}queue;
+
+queue *init_queue(int max_size);
+void enqueue(queue* q,void *data);
+void dequeue(queue* q,void **data);
+#endif
---
+++ libevent-buffer/client/src/queue.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+queue *init_queue(int max_size)
+{
+    queue *q = (queue *)malloc(sizeof(queue));
+    if(q == NULL)
+    {
+	perror("malloc failed \n");
+	return NULL;
+    }
+    q->data = (void **)malloc(max_size*sizeof(void *));
+    if(q->data == NULL)
+    {
+	perror("malloc failed \n");
+	return NULL;
+    }
+
+    q->_head = 0;
+    q->_tail = 0;
+    q->max_size = max_size;
+
+    pthread_mutex_init(&q->lock, NULL);  
+    pthread_cond_init(&q->notempty, NULL);  
+    pthread_cond_init(&q->notfull, NULL);  
+    return q;
+}
+
+void enqueue(queue* q,void *data)
+{
+     pthread_mutex_lock(&q->lock);  
+    /* 等待缓冲区未满*/  
+    if ((q->_tail + 1) % QUEUE_SIZE == q->_head)  
+    {  
+        pthread_cond_wait(&q->notfull, &q->lock);  
+    }  
+    /* 写数据,并移动指针 */  
+    q->data[q->_tail] = data;  
+    q->_tail++;  
+    if (q->tail >= QUEUE_SIZE)  
+        q->tail = 0;  
+    /* 设置缓冲区非空的条件变量*/  
+    pthread_cond_signal(&q->notempty);  
+    pthread_mutex_unlock(&q->lock);
+}
+
+void dequeue(queue* q,void **data)
+{
+    pthread_mutex_lock(&q->lock);
+    /* 等待缓冲区未满*/
+    if (q->_tail == q->_head)      
+    {
+        pthread_cond_wait(&q->notempty, &q->lock);
+    }
+    /* 写数据,并移动指针 */
+    *data = q->data[q->_head];     
+    q->_head++;   
+    if (q->head >= QUEUE_SIZE)     
+        q->head = 0;    
+    /* 设置缓冲区非空的条件变量*/
+    pthread_cond_signal(&q->notfull);
+    pthread_mutex_unlock(&q->lock);
+}
